cc71348d0568b2ca71aa35a748c9ea160437c143
haikaljs <haikaljavascript@gmail.com>
2024-12-05 22:04:23 +0800

    Admin create: implement image picker

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,14 +1,17 @@
 import { StyleSheet, Text, View, TextInput, Image } from "react-native";
 import React, { useState } from "react";
+import * as ImagePicker from "expo-image-picker";
 import Button from "@/src/components/Button";
 import { defaultPizzaImage } from "@/src/components/ProductListItem";
 import Colors from "@/src/constants/Colors";
+import { Stack } from "expo-router";
 
 const CreateProductScreen = () => {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
-
   const [errors, setErrors] = useState("");
+
+  const [image, setImage] = useState<string | null>(null);
 
   const resetFields = () => {
     setName("");
@@ -48,18 +51,45 @@ const CreateProductScreen = () => {
     resetFields();
   };
 
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Image source={{ uri: defaultPizzaImage }} style={styles.image} />
-      <Text style={styles.textButton}>Select Image</Text>
+      <Stack.Screen options={{ title: "Create Product" }} />
+      <Image
+        source={{ uri: image || defaultPizzaImage }}
+        style={styles.image}
+      />
+
+      <Text onPress={pickImage} style={styles.textButton}>
+        Select Image
+      </Text>
+
       <Text style={styles.label}>Name</Text>
+
       <TextInput
         value={name}
         onChangeText={setName}
         placeholder="Name"
         style={styles.input}
       />
+
       <Text style={styles.label}>Price ($)</Text>
+
       <TextInput
         value={price}
         onChangeText={setPrice}
@@ -67,7 +97,8 @@ const CreateProductScreen = () => {
         style={styles.input}
         keyboardType="numeric"
       />
-      <Text style={{ color: "red" }}>{errors}</Text>;
+      <Text style={{ color: "red" }}>{errors}</Text>
+
       <Button onPress={onCreate} text="Create" />
     </View>
   );
@@ -90,7 +121,7 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     fontWeight: "bold",
     color: Colors.light.tint,
-    marginVertical: 10
+    marginVertical: 10,
   },
   input: {
     backgroundColor: "white",
