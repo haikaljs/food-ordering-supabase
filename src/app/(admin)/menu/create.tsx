b8810c231ab0818f4c15dc6392ee00731a9d79e9
haikaljs <haikaljavascript@gmail.com>
2024-12-05 21:37:58 +0800

    Admin create menu: text input, form

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,10 +1,70 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import React, { useState } from "react";
+import Button from "@/src/components/Button";
 
 const CreateProductScreen = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const [errors, setErrors] = useState("");
+
+  const resetFields = () => {
+    setName("");
+    setPrice("");
+  };
+
+  const validateInput = () => {
+    setErrors("");
+    if (!name) {
+      setErrors("Name is required");
+      return false;
+    }
+    if (!price) {
+      setErrors("Price is required");
+      return false;
+    }
+    if (isNaN(parseFloat(price))) {
+      setErrors("Price is not a number");
+      return false;
+    }
+
+    return true;
+  };
+
+  const onCreate = () => {
+    // validate
+    if (!validateInput()) {
+      return;
+    }
+    console.warn("Creating product:", name);
+    console.warn("Creating product:", price);
+
+    // Save in db
+
+    // Display error message
+
+    resetFields();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>CreateProductScreen</Text>
+      <Text style={styles.label}>Name</Text>
+      <TextInput
+        value={name}
+        onChangeText={setName}
+        placeholder="Name"
+        style={styles.input}
+      />
+      <Text style={styles.label}>Price ($)</Text>
+      <TextInput
+        value={price}
+        onChangeText={setPrice}
+        placeholder="9.99"
+        style={styles.input}
+        keyboardType="numeric"
+      />
+      <Text style={{ color: "red" }}>{errors}</Text>;
+      <Button onPress={onCreate} text="Create" />
     </View>
   );
 };
@@ -15,6 +75,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    padding: 10
+    padding: 10,
+  },
+  input: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 5,
+    marginBottom: 20,
+  },
+  label: {
+    color: "grey",
+    fontSize: 16,
   },
 });
